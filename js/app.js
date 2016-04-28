18dda136607ee0aa5a6b1fa01042f886667bace9
Kyle <bigbanner@gmail.com>
2016-04-27 23:43:23 -0500

    First working CRUD + front end combo

--- js/app.js
+++ js/app.js
@@ -8,19 +8,6 @@ const component = omniscient.withDefaults({
 
 $(document).ready(function(){
   $('.slider').slider({full_width: true});
-  $.ajax({
-   type: 'GET',
-    url: 'http://localhost:3666/events',
-    headers : {
-      "accept" : "application/json"
-    },
-    success: function(json) {
-       $('body').append("<img src=\"" + json[0].image + "\" />");
-    },
-    error: function(e) {
-       console.log(e.message);
-    }
-});
 });
 
 const App = component('App', ({ counter }) => {
--- js/eventSlides.js
+++ js/eventSlides.js
@@ -6,27 +6,35 @@ import eventsArray from './eventInfo';
 const Slides = component('Slides', {
 
 }, () => {
-  var activeEvents = [];
-  eventsArray.forEach(event => {
-    if(event.eventTitle !== ""){
-      event.backgroundImageStyle = {backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('+event.eventPicture+')'};
-      activeEvents.push(event);
+  var eventInfo = [];
+  $.ajax({
+    async: false,
+    type: 'GET',
+    url: 'http://localhost:3666/events',
+    headers : {
+      "accept" : "application/json"
+    },
+    success: function(json) {
+       eventInfo = json;
+    },
+    error: function(e) {
+       console.log(e.message);
     }
   });
 
   let i = 0;
-
-  const eventsInstance = activeEvents.map(event =>
+  const eventsInstance = eventInfo.map(event =>
     <li key={i++}>
-      <img src={event.eventPicture} className="filterImg"></img>
+      <img src={event.image} className="filterImg"></img>
       <div className="caption center-align">
-          <h3>{event.eventTitle}</h3>
-          {event.eventCaption === "" ? "" : <h5 className="light text-lighten-3">{event.eventCaption}</h5>}
-          {event.eventInfo === "" ? "" : <h5 className="light text-lighten-3">{event.eventInfo}</h5>}
-          {event.eventLink === "" ? "" : <h5 className="light text-lighten-3">{event.eventLink}</h5>}
+          <h3>{event.title}</h3>
+          {event.caption === "" ? "" : <h5 className="light text-lighten-3">{event.caption}</h5>}
+          {event.info === "" ? "" : <h5 className="light text-lighten-3">{event.info}</h5>}
+          {event.link === "" ? "" : <h5 className="light text-lighten-3">{event.link}</h5>}
       </div>
     </li>
   );
+  
   return(
     <div className="slider fullscreen events-slider"><ul className="slides">{eventsInstance}</ul></div>
   );
@@ -34,36 +42,3 @@ const Slides = component('Slides', {
 });
 
 export default Slides;
-
-// <div class="slider">
-  // <ul class="slides">
-  //   <li>
-  //     <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
-  //     <div class="caption center-align">
-  //       <h3>This is our big Tagline!</h3>
-  //       <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-  //     </div>
-  //   </li>
-  //   <li>
-  //     <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
-  //     <div class="caption left-align">
-  //       <h3>Left Aligned Caption</h3>
-  //       <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-  //     </div>
-  //   </li>
-  //   <li>
-  //     <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
-  //     <div class="caption right-align">
-  //       <h3>Right Aligned Caption</h3>
-  //       <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-  //     </div>
-  //   </li>
-  //   <li>
-  //     <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
-  //     <div class="caption center-align">
-  //       <h3>This is our big Tagline!</h3>
-  //       <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-  //     </div>
-  //   </li>
-  // </ul>
-// </div>
